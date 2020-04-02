61954c874000b3e4777fa5a800994f09d6ec7d6f
Vivek <vivekmunde@gmail.com>
2020-04-02 16:31:59 +0530

    typo fix

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-import createSubscription from './src/create-publication';
+import createPublication from './src/create-publication';
 import publish from './src/publish';
 import subscribe from './src/subscribe';
 import withPublish from './src/with-publish';
 import withSubscribe from './src/with-subscribe';
 
 export {
-    createSubscription,
+    createPublication,
     publish,
     subscribe,
     withPublish,
