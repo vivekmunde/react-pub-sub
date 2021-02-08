7fdb952b8060bbb620576a3f556a3e232f5db134
Vivek <vivekmunde@gmail.com>
2021-02-08 16:44:32 +0530

    source map for development mode

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import { createPublication, publish, subscribe } from 'pusu';
+import withSubscribe from './with-subscribe';
+import useSubscribe from './use-subscribe';
+
+export {
+  createPublication,
+  publish,
+  subscribe,
+  withSubscribe,
+  useSubscribe
+};
