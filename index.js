bd9bde8bdce88078499dea57c439621438f9df12
Vivek <vivekmunde@gmail.com>
2021-09-10 17:03:09 +0530

    main lib path

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
 'use strict';
 
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./dist/react-pusu.production.js');
-} else {
-  module.exports = require('./dist/react-pusu.development.js');
-}
+module.exports = require('./lib/cjs/index.js');
